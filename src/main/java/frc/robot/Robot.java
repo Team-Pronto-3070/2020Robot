9fe937e684af2033a7d2e07962931f3e9cd100a3
FRCTeam3070 <59633079+FRCTeam3070@users.noreply.github.com>
2020-01-16 18:30:42 -0800

    Fixed the merge errors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
+public class Robot extends TimedRobot implements i_Pronstants{
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
