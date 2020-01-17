06ef464aed5baeb2f4d8603968d3689addf7caff
FRCTeam3070 <59633079+FRCTeam3070@users.noreply.github.com>
2020-01-16 17:53:06 -0800

    Sendable Chooser for Auto,

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -21,6 +23,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  SendableChooser<String> initPos = SendableChooser<String>();
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -28,9 +31,18 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+
+    initPos.addOption("Left", "R");
+    initPos.addOption("Middle", "M");
+    initPos.addOption("Preferred", "P");
+    initPos.addOption("Right", "L");
+
+    SmartDashboard.putData(initPos);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+
   }
 
   /**
