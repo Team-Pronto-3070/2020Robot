444772709c6aa19e3d2dc6f103834509db8d45c7
FRCTeam3070 <3070executive@gmail.com>
2020-01-21 16:25:30 -0800

    Made climber constructor

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,16 +1,20 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 class Climber extends SubsystemBase{
 
-    TalonSRX t_Winch = new TalonSRX(RobotMap.T_WINCH_PORT);
-    TalonSRX t_TeleArm = new TalonSRX(RobotMap.T_TELE_PORT);
+    TalonSRX t_Winch;
+    TalonSRX t_TeleArm;
     
     public Climber(){
+        t_Winch = new TalonSRX(RobotMap.T_WINCH_PORT);
+        t_TeleArm = new TalonSRX(RobotMap.T_TELE_PORT);
+    }
+
+    public void goUp(){
         
     }
 }
