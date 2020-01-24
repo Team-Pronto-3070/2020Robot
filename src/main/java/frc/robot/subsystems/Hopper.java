53ef1b47874f8bc8da31e9b64fed56e7ebf9f57f
FRCTeam3070 <59633079+FRCTeam3070@users.noreply.github.com>
2020-01-23 16:11:31 -0800

    Update Hopper.java

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,8 +12,6 @@ class Hopper extends SubsystemBase {
     public Hopper(){
         t_Hopper = new TalonSRX(RobotMap.T_HOPPER_PORT);
         
-    
-    
     }
 
     public void goUp(){
@@ -20,15 +19,13 @@ class Hopper extends SubsystemBase {
        
     }
 
-    //Retracts arm and winch. They do not need to be synced.
-    public void goDown(){
+    public void in(){
         t_Hopper.set(ControlMode.PercentOutput, -1);
        
     }
 
-    //Stops both motors.
-    public void stop(){
-        t_Hopper.set(ControlMode.PercentOutput,0);
+    public void out(){
+        t_Hopper.set(ControlMode.PercentOutput,1);
         
     }
 
