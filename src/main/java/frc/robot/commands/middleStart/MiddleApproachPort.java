d68c3b8def5533d7d7194bba484fe648e1eba5db
FRCTeam3070 <59633079+FRCTeam3070@users.noreply.github.com>
2020-01-30 17:06:47 -0800

    rightStart

---
+++ src/main/java/frc/robot/commands/middleStart/MiddleApproachPort.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.LeftStart;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotMap;
+import frc.robot.subsystems.Drivetrain;
+
+public class LeftApproachPort extends CommandBase {
+    
+    private Drivetrain drive;
+    
+    public LeftApproachPort(Drivetrain dt){
+        drive = dt;
+        addRequirements(dt);
+    }
+}
