f418b6a700b195fbff4a8e2848e231b9dcdca19e
FRCTeam3070 <59633079+FRCTeam3070@users.noreply.github.com>
2020-02-03 15:51:00 -0800

    Finishing up the intake class

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,13 +7,17 @@ import frc.robot.RobotMap;
 
 public class Intake extends SubsystemBase {
 
+    TalonSRX t_intake1;
+    TalonSRX t_intake2;
     public Intake(){
-        TalonSRX t_intake1 = new TalonSRX(RobotMap.T_BELT1_PORT); 
-        TalonSRX t_intake2 = new TalonSRX(RobotMap.T_BELT2_PORT);
+        t_intake1 = new TalonSRX(RobotMap.T_BELT1_PORT); 
+        t_intake2 = new TalonSRX(RobotMap.T_BELT2_PORT);
     
     
     }
 
-    
+    public void intake(){
+        t_intake1.set(ControlMode.PercentOutput, 1);
+    }
 
 }
