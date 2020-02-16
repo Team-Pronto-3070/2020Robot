e754bbbcfc8816b84e0daf1e419cfcd5d346d615
beechnutkjr <xseggs@gmail.com>
2020-02-15 17:40:28 -0800

    Housekeeping

--- src/main/java/frc/robot/commands/ClimberStop.java
+++ src/main/java/frc/robot/commands/ClimberStop.java
@@ -6,21 +6,13 @@ import frc.robot.subsystems.Climber;
 public class ClimberStop extends CommandBase {
 
     private Climber climb;
-    
 
     public ClimberStop(Climber climber){
-            climb = climber;
-            addRequirements(climber);
-
-            
+        climb = climber;
+        addRequirements(climber);
     }
 
     public void execute(){
-      
-            climb.stop();
-        
-    }
-
-
-    
+        climb.stop();
+    }   
 }
