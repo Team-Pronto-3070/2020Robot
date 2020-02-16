ae239afddce6e78a6593551e9a6a2ed3d5500003
beechnutkjr <xseggs@gmail.com>
2020-02-15 16:10:31 -0800

    Housekeeping

--- src/main/java/frc/robot/commands/ClimberUp.java
+++ src/main/java/frc/robot/commands/ClimberUp.java
@@ -1,49 +1,23 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
 public class ClimberUp extends CommandBase {
-    Timer timer = new Timer();
     private Climber climber;
 
-    //DigitalInput upperLimit;
-
-    
     public ClimberUp (Climber climb) {
         climber = climb;
         addRequirements(climb);
     }
     public void execute(){ 
-        
-          //winch and climber up, climber down and then winch down
-
-          //TODO: need to make sure limit switches will stop the motors before testing
-        // if(!upperLimit.get()){ 
-            climber.goUp();
-       // }
-        
-        
+        climber.goUp();
         
     }
-    public boolean isLimit(){
-        //return upperLimit.get();
-        return false;
-    }
-
+    
     public boolean isFinished() {
         return false;
-       }
-       protected void end(){
-       }
-       protected void initalize(){
-       }
-       protected void interrupted(){
-        // Robot.drive.stop();
-}
+    }
 }
 
 
