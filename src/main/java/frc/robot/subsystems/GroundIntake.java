25b46c6fc00267265cf7eb313a1e34fe73b53d6b
FRCTeam3070 <59633079+FRCTeam3070@users.noreply.github.com>
2020-02-17 15:52:11 -0800

    Comments and nmerge error resolution

--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -6,22 +6,26 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
-public class GroundIntake extends SubsystemBase {
+//this is the program on how the robot picks up balls on the ground.
+//the motors, when running, will run a belt which will pull balls along it, depositing them into the hopper.
 
-    TalonSRX t_intake1;
+public class GroundIntake extends SubsystemBase {
+//GroundIntake is part of the subsystems folder.
+
+    TalonSRX t_intake1; //selects the talonports t_intake1 and t_intake2.
     TalonSRX t_intake2;
-    public GroundIntake(){
-        t_intake1 = new TalonSRX(RobotMap.T_BELT1_PORT); 
-        t_intake2 = new TalonSRX(RobotMap.T_BELT2_PORT);
+    public GroundIntake(){ //constructor
+        t_intake1 = new TalonSRX(RobotMap.T_BELT1_PORT); //defines the ports as a talon, connecting them to the variable in RobotMap
+        t_intake2 = new TalonSRX(RobotMap.T_BELT2_PORT); 
     }
 
     public void intake(){
-        t_intake1.set(ControlMode.PercentOutput, 1);
-        t_intake2.set(ControlMode.PercentOutput, 1);
+        t_intake1.set(ControlMode.PercentOutput, 1); //gives the intake motors the value of 100%, turning them on.
+        t_intake2.set(ControlMode.PercentOutput, 1); //it makes them run the belt, moving the balls.
     }
 
     public void stop(){
-        t_intake1.set(ControlMode.PercentOutput, 0);
+        t_intake1.set(ControlMode.PercentOutput, 0);//this stops the motors, stoppin the belt.
         t_intake2.set(ControlMode.PercentOutput, 0);
     }
 
