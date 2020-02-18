e80d877cd079c7dc3cf78ad5d3bc58ac4eaf5083
beechnutkjr <xseggs@gmail.com>
2020-02-18 12:56:03 -0800

    Final button bindings

--- src/main/java/frc/robot/commands/ClimberCommmands/ToggleClimber.java
+++ src/main/java/frc/robot/commands/ClimberCommmands/ToggleClimber.java
@@ -3,17 +3,17 @@ package frc.robot.commands.ClimberCommmands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
-public class ClimberDown extends CommandBase {
+public class ToggleClimber extends CommandBase {
 
     private Climber climb;
 
-    public ClimberDown(Climber climber){
+    public ToggleClimber(Climber climber){
             climb = climber;
             addRequirements(climber);
     }
 
     public void execute(){
-        climb.goDown();
+        climb.toggle();
     }
 
     public boolean isFinished(){
